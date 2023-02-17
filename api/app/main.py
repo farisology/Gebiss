369398b0096681f171f320c512805e3838af6942
super <super@STREAK.myguest.virtualbox.org>
2023-02-18 02:48:13 +0800

    api artefacts

--- api/app/main.py
+++ api/app/main.py
@@ -1,32 +1,69 @@
-# import torch
+import os
+import joblib
+import pandas as pd
+import lightgbm as lgb
 from fastapi import FastAPI
-# from .utility import inference
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from typing import Union, List
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
-class DetectedObject(BaseModel):
-    xmin: float
-    # ymin: float
-    # xmax: float
-    # ymax: float
-    # confidence: float
-    # detected_class: int = Field(alias='class')
-    # name: str
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="DÜRR DENTAL test CarPrice API",
+        version="0.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://www.duerrdental.net/fileadmin/template/img/Logo_DD_EN_RGB.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
-class DetectionResponse(BaseModel):
-    milage: float
-    fuel_type: str
-    offer_type: str
-    horse_power: float
+app.openapi = custom_openapi
+
+
+class PreidictionObject(BaseModel):
+    price: float
+
+class CarObjectRequest(BaseModel):
+    milage: int
+    make: str
+    model: str
+    fuel: str
+    gear: str
+    offerType: str
+    hp: int
+    year: int
 
 @app.get("/")
 def read_root():
     return {"message": "you are here for a reason, follow your heart."}
 
-# main endpoint for object detection using yolo5 small model
-@app.get("/predict_price", response_model=DetectedObject)
-async def predict(image_url:str):
-    # objects = inference(image_url)
-    return {"xmin": 0.0}
+# main endpoint for car price prediction model
+@app.get("/predict_price", response_model=PreidictionObject)
+async def predict(request: CarObjectRequest):
+    # load model
+    cwd = os.getcwd()
+    gbm = joblib.load(f'{cwd}/app/german_cars_pricing_model.pkl')
+
+    # preprocess the request data to model compatible format
+    mydict = dict()
+    for e in request:
+        mydict[e[0]] = [e[1]]
+    # print(mydict)
+    query_record = pd.DataFrame.from_records(mydict)
+
+    # Casting into categorical just like we did in the training
+    obj_feat = list(query_record.loc[:, query_record.dtypes == 'object'].columns.values)
+    for feature in obj_feat:
+        query_record[feature] = pd.Series(query_record[feature], dtype="category")
+
+    # model making predictions
+    price = gbm.predict(query_record)
+
+    return {"price": price}
